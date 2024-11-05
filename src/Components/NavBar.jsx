d9e46c1bae40e764f1d15bc616f51fe1c92bd675
Kar9ti8k <karthiksankineni07@gmail.com>
2024-11-05 18:43:04 +0530

    NavBar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,27 +4,50 @@ import { Link } from 'react-router-dom'
 import SideBar from './SideBar'
 import './NavBar.css'
 import { useGlobalContext } from './Context'
-
+import { useLocation } from 'react-router-dom'
 const NavBar = () => {
   const { openSideBar } = useGlobalContext()
-
+  const locaion = useLocation()
   return (
     <>
       <nav className='Navbar'>
         <div className='Nav-main'>
-          <Link to='/' className='Nav-Link'>
+          <Link
+            to='/'
+            className={`Nav-Link ${location.pathname === '/' ? 'active' : ''}`}
+          >
             <p className='hidden sm:inline'>Home</p>
           </Link>
-          <Link to='/Process' className='Nav-Link'>
+          <Link
+            to='/Process'
+            className={`Nav-Link ${
+              location.pathname === '/Process' ? 'active' : ''
+            }`}
+          >
             <p className='hidden sm:inline'>Process</p>
           </Link>
-          <Link to='/Services' className='Nav-Link'>
+          <Link
+            to='/Services'
+            className={`Nav-Link ${
+              location.pathname === '/Services' ? 'active' : ''
+            }`}
+          >
             <p className='hidden sm:inline'>Services</p>
           </Link>
-          <Link to='/JobPositions' className='Nav-Link'>
+          <Link
+            to='/JobPositions'
+            className={`Nav-Link ${
+              location.pathname === '/JobPositions' ? 'active' : ''
+            }`}
+          >
             <p className='hidden sm:inline'>JobPositions</p>
           </Link>
-          <Link to='/Contact' className='Nav-Link'>
+          <Link
+            to='/Contact'
+            className={`Nav-Link ${
+              location.pathname === '/Contact' ? 'active' : ''
+            }`}
+          >
             <p className='hidden sm:inline'>Contact</p>
           </Link>
         </div>
